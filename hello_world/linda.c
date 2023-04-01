20a749ce0df4eacdb59eb4786dfe6451a2591669
root <lindacabreracastro@gmail.com>
2023-04-01 05:13:22 -0700

    11

---
+++ hello_world/linda.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - Run a C  file preprocessor and save into another file
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("Hi linda\n");/*inline comment*/
+printf("How nice to see you here\n");
+return (0);
+}
