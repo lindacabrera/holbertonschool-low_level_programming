4c76261c5681d36b6c8fcbd823957bebb17c5360
root <lindacabreracastro@gmail.com>
2023-04-08 02:59:57 -0700

    11

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,11 +13,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 	putchar(i + '0');
+	if (i != 9)
+	{
 	putchar(',');
 	putchar(' ');
+	}
 	}
 putchar('\n');
 return (0);
