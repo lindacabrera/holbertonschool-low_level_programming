49f381478383b51577ce3c18dc5f672fc2375613
root <lindacabreracastro@gmail.com>
2023-04-07 23:23:06 -0700

    11

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - I sometimes suffer from insomnia.
+ *
+ * Description: I never thought that it would pay off
+ *
+ * Parameter: ch
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+		for (i = 0; i <= 9 ; i++)
+		{
+			putchar(i + '0');
+		}
+	putchar('\n');
+return (0);
+}
