2d6c180a8c527e7c8a0dc46c968c3f77e163d6da
root <lindacabreracastro@gmail.com>
2023-04-07 23:06:45 -0700

    11

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - I sometimes suffer from insomnia.
+ *
+ * Description: I never thought that it would pay off
+ *
+ * Parameter: ch
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+		for (n = 0; n <= 9; n++)
+		{
+		printf("%d", n);
+		}
+		putchar('\n');
+return (0);
+}
