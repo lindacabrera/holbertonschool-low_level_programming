78e37fc4c91011ea29f5aed006ca679d497c21e1
root <lindacabreracastro@gmail.com>
2023-04-09 00:48:38 -0700

    11

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+ * main - Print _putchar
+ *
+ *Description: This program prints _putchar
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+		printf("_putchar\n");
+	return (0);
+}
