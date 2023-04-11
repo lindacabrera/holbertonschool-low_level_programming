85e93c4228ad11c856be63ecca665cdcc0ef1af9
root <lindacabreracastro@gmail.com>
2023-04-11 00:37:02 -0700

    11

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - Absolute
+ *
+ * Description: This function prints the sign of a number
+ *
+ * @num: num is the parameter
+ *
+ * Return: Always 0.
+ */
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
+}
