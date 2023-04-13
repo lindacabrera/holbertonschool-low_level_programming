1829d0b41e3f0f8166a4d7803926489098d41d23
root <lindacabreracastro@gmail.com>
2023-04-13 04:20:05 -0700

    11

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two values
+ *
+ * Description: this program adds two integers and returns the result
+ *
+ * @a: first Parameter
+ * @b: Second parameter
+ *
+ * Return: value a+b
+ */
+
+int add(int a, int b)
+{
+return  (a + b);
+}
