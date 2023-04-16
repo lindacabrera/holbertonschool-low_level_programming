a7798085fbae34548766eb1074e029a1bd9f1868
root <lindacabreracastro@gmail.com>
2023-04-16 04:13:48 -0700

    11

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -16,16 +16,16 @@ int print_last_digit(int n)
 	int last_d = 0;
 	int abs_n = 0;
 
-	if (n < 0)
+	last_d = n % 10;
+
+	if (last_d < 0)
 	{
-		abs_n = n * -1;
-		last_d = abs_n % 10;
-		_putchar(last_d + '0');
-		return (last_d);
+		abs_n = last_d * (-1);
+		_putchar(abs_n + '0');
+		return (abs_n);
 	}
 	else
 	{
-		last_d = n % 10;
 		_putchar(last_d + '0');
 		return (last_d);
 	}
