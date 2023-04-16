b62f2cae11585eaf55e5eb8338efa70e4643dc03
root <lindacabreracastro@gmail.com>
2023-04-16 03:02:30 -0700

    11

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz
+ *
+ * Description: This program prints the numbers from 1 to 100 avoiding 3 and 5 x
+ *
+  */
+
+int main(void)
+{
+	int i;
+	int t_m = 0;
+	int f_m = 0;
+
+	for (i = 1; i < 101; i++)
+	{
+		t_m = i % 3;
+		f_m = i % 5;
+
+		if (t_m == 0 && f_m == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else
+		{
+			if (t_m == 0)
+			{
+				printf("Fizz");
+				printf(" ");
+			}
+			else
+			{
+				if (f_m == 0)
+				{
+					printf("Buzz");
+					printf(" ");
+				}
+				else
+				{
+					printf("%d",i);
+					printf(" ");
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
