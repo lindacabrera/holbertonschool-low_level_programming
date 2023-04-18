b149c3870b12e9a1562a27381daa3cf55320ea4c
root <lindacabreracastro@gmail.com>
2023-04-18 03:38:06 -0700

    11

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - 98 Battery st
+ *
+ * Description: takes a pointer to an int as parameter and updates value 98
+ *
+ * @n: parameter
+ *
+  */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
