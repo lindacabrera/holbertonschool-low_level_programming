b659f2ef13579c80e37edb6eff68402889d5d411
root <lindacabreracastro@gmail.com>
2023-04-20 01:34:46 -0700

    11

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - This report, by its very length, defends itself against risk...
+ *
+ * Description: This function that returns the length of a string.
+ *
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; s[a] != '\0'; a++)
+	{
+	}
+	return (a);
+}
