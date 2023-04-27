277b6957802cdab26bbdb76afc318562a832d3f4
root <lindacabreracastro@gmail.com>
2023-04-27 05:19:54 -0700

    12

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  * *_strncpy - strncpy
+  *
+  * Description: This function copies a string
+  *
+  * @dest: string destine
+  *
+  * @src : string to copy
+  *
+  * @n : leng of the array
+  *
+  * Return: string conpied.
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
