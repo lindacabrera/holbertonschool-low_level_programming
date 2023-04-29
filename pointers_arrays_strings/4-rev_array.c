84db8aa8ff1cb1be2d97e4845c3c84aae50fa167
root <lindacabreracastro@gmail.com>
2023-04-29 02:18:31 -0700

    12

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * reverse_array - reverse funtion
+  *
+  * Description: This function reverses the content of an array of integers
+  *
+  * @a: string destine
+  *
+  * @n : bytes for string
+  *
+  * Return: Nothing
+  */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = n - 1;
+	int temp = 0;
+
+	for (i = 0; i <= n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] =  temp;
+		j--;
+	}
+}
