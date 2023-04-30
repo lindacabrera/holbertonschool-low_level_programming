2804d449cb9c30672b27af72a21216f82a03d57c
root <lindacabreracastro@gmail.com>
2023-04-30 01:24:34 -0700

    13

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * print_chessboard - Chess is mental torture
+  *
+  * Description: This function that prints the chessboard.
+  *
+  * @a: array of array to print
+  *
+  * Return:  Nothing
+  */
+
+void print_chessboard(char (*a)[8])
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y]);
+		}
+		_putchar('\n');
+	}
+}
