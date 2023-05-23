90f908e9b6cd6f0303d36f2cb24907bf46feea8f
root <lindacabreracastro@gmail.com>
2023-05-23 06:52:25 -0700

    16

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+  * main - name funtion
+  *
+  * Description: This program prints the sum of the numbers.
+  *
+  * @argc: Number of arguments
+  *
+  * @argv: Array of arguments beggining with its name
+  *
+  * Return: 0 if success
+  */
+
+int main(int argc, char *argv[])
+{
+	int j;
+	int sum = 0;
+
+	for (j = 1; j < argc; j++)
+	{
+		if (argc == 1)
+		{
+			return (0);
+		} else if ('0' > *argv[j] || *argv[j] > '9')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[j]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
