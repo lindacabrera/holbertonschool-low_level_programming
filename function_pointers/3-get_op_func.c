c951d9ee3423568bda05df372679c20a76f67b38
root <lindacabreracastro@gmail.com>
2023-06-19 03:23:16 -0700

    18

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -28,13 +28,14 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (i < 10)
+	while (i < 5)
 	{
-		if (*s == ops->op[i])
+		if (s[0] == ops[i].op[0] && s[1] == ops[i].op[1])
 		{
+
 			break;
 		}
 		i++;
 	}
-	return (ops[i / 2].f);
+	return (ops[i].f);
 }
